0de1b750c876276de55be9cdb9c6a2f52856bb25
jegarba <jegarba1996@gmail.com>
2020-12-10 09:29:24 -0500

    agregado atributo cedula

--- src/main/java/com/clearminds/jg/dtos/Estudiante.java
+++ src/main/java/com/clearminds/jg/dtos/Estudiante.java
@@ -4,20 +4,25 @@ public class Estudiante {
 	private int Id;
 	private String Nombre;
 	private String Apellido;
+	private int Cedula;
 
 	public Estudiante(){
 		
 	}
-	
-	public Estudiante(String nombre, String apellido) {
+
+	public Estudiante(String nombre, String apellido, int cedula) {
+		super();
 		Nombre = nombre;
 		Apellido = apellido;
+		Cedula = cedula;
 	}
 
-	public Estudiante(int id, String nombre, String apellido) {
+	public Estudiante(int id, String nombre, String apellido, int cedula) {
+		super();
 		Id = id;
 		Nombre = nombre;
 		Apellido = apellido;
+		Cedula = cedula;
 	}
 
 	public int getId() {
@@ -44,9 +49,18 @@ public class Estudiante {
 		Apellido = apellido;
 	}
 
+	public int getCedula() {
+		return Cedula;
+	}
+
+	public void setCedula(int cedula) {
+		Cedula = cedula;
+	}
+
 	@Override
 	public String toString() {
-		return "Estudiante [Id=" + Id + ", Nombre=" + Nombre + ", Apellido=" + Apellido + "]";
+		return "Estudiante [Id=" + Id + ", Nombre=" + Nombre + ", Apellido=" + Apellido + ", Cedula=" + Cedula + "]";
 	}
+	
 
 }
